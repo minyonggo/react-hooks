f79584e27a23cd5e78aa96d69d615a932eea51d6
Minyong#2020.06.18 <ricky23@yonsei.ac.kr>
2020-06-19 00:19:28 +0900

    #1.3 useInput part Two(revised)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import useInput from './useInput';
-
-
-
-
 
 const App = () => {
   const emailSet = useInput("");
--- src/useInput.js
+++ src/useInput.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 const useInput = (initialValue) => {
     const [value, setInput] = useState(initialValue);
 
