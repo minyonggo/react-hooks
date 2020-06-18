b52b943c9cc30277923b1b9228ad808ea73eddf0
Minyong#2020.06.18 <ricky23@yonsei.ac.kr>
2020-06-18 23:49:01 +0900

    #1.0 Introduction to useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello! {count}</h1>
+      <button onClick={() => setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count-1)}>Decrement</button>
     </div>
   );
 }
